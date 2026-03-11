8608d2920c42134b055c17fd180ded59f3c7bf89
Gasova Elizaveta <cdyhdrxj@yandex.ru>
2026-03-11 22:36:27 +0300

    Запуск сервисов через docker-compose и первая миграция

--- packages/db/models/base.py
+++ packages/db/models/base.py
@@ -1,12 +1,3 @@
-"""Base classes and imports for SQLAlchemy models"""
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import (
-    Column, BigInteger, String, Text, Boolean, Integer, Float,
-    ForeignKey, UniqueConstraint, JSON, Enum as SQLEnum,
-    TIMESTAMP
-)
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-import enum
 
 Base = declarative_base()
