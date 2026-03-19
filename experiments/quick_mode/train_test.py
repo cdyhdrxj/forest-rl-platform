2054b8f2db73058b6784e5745fd62fafc47e4e8d
Yana0102 <128310255+Yana0102@users.noreply.github.com>
2026-03-19 20:59:15 +0300

    Update train_test.py

--- experiments/quick_mode/train_test.py
+++ experiments/quick_mode/train_test.py
@@ -1,23 +1,40 @@
 from stable_baselines3 import SAC
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
-from forest_env import ForestEnv
 from stable_baselines3.common.monitor import Monitor
+
+from forest_env import ForestEnv
 
 
 env = DummyVecEnv([lambda: Monitor(ForestEnv())])
-env = VecNormalize.load("vec_normalize.pkl", env)
-env.training = True
-env.norm_reward = True
 
-model = SAC.load("sac_forest_v1", env=env)
+env = VecNormalize(
+    env,
+    norm_obs=True,
+    norm_reward=True
+)
+
+model = SAC(
+    "MlpPolicy",
+    env,
+    learning_rate=3e-4,
+    buffer_size=300000,
+    batch_size=256,
+    gamma=0.99,
+    tau=0.005,
+    train_freq=1,
+    gradient_steps=1,
+    verbose=1,
+    tensorboard_log="./tensorboard/"
+)
 
 model.learn(
-    total_timesteps=20_000,
+    total_timesteps=100000,
     log_interval=3,
     progress_bar=True
 )
 
-model.save("sac_forest_v2")
+model.save("sac_forest_v1")
+
 env.save("vec_normalize.pkl")
 
 env.close()
